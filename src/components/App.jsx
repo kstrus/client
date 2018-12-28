fdb81443db85aeb80c0fea778371ea72d306171a
kstrus <katarzyna.anna.strus@gmail.com>
2018-12-28 21:36:51 +0100

    Add react-router; create navigation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './Header';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
 
 class App extends React.Component {
     render() {
-        return <div>Client App</div>;
+        return (
+            <div className="ui container">
+                <BrowserRouter>
+                    <div>
+                        <Header />
+                        <Route path="/" exact component={StreamList} />
+                        <Route path="/streams/show" component={StreamShow} />
+                        <Route path="/streams/new" component={StreamCreate} />
+                        <Route path="/streams/edit" component={StreamEdit} />
+                        <Route path="/streams/delete" component={StreamDelete} />
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
     }
 }
 
